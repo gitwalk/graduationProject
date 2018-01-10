24ddb53de9460a4dd42fb4fa38a9d4fea77d919e
TT <987719481@qq.com>
2018-01-10 21:12:15 +0800

    Revert "继续修改vue样式错位的bug"
    This reverts commit 5667ad9ad53171cdb4d4c81f323a91912c0ebe4d.

--- vue-element/src/page/admin/Login.vue
+++ vue-element/src/page/admin/Login.vue
@@ -45,15 +45,10 @@
 <!--<script src="/static/js/EasePack.min.js"></script>-->
 <!--<script src="/static/js/rAF.js"></script>-->
 <!--<script src="../../../static/js/demo-1.js"></script>-->
-
-<style scoped src="../../../static/css/normalize.css"></style>
-<style scoped src="../../../static/css/demo.css"></style>
-<style scoped src="../../../static/css/component.css"></style>
-
 <style scoped>
-  /*@import "../../../static/css/normalize.css";*/
-  /*@import "../../../static/css/demo.css";*/
-  /*@import "../../../static/css/component.css";*/
+  @import "../../../static/css/normalize.css";
+  @import "../../../static/css/demo.css";
+  @import "../../../static/css/component.css";
 
   a:hover{
     cursor:pointer
